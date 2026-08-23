c511bc98840c658454593eaafe154e5cb97a96ef
Abdullahi Salisu <abdulkhan07041@gmail.com>
2026-08-23 16:21:39 +0100

    Use Firebase environment credentials

--- firebase.js
+++ firebase.js
@@ -1,7 +1,13 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./global-express-data-firebase-adminsdk-fbsvc-12a5f8adf4.json");
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        : undefined
+};
 
 initializeApp({
     credential: cert(serviceAccount)
